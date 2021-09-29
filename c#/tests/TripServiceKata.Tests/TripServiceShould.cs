5a0175b4b93ba51bea1b42192d2446df95825fab
jonayrb <jonayro@hotmail.com>
2021-09-29 07:35:03 +0100

    add test get_trips_by_user

--- c#/tests/TripServiceKata.Tests/TripServiceShould.cs
+++ c#/tests/TripServiceKata.Tests/TripServiceShould.cs
@@ -1,6 +1,20 @@
-﻿namespace TripServiceKata.Tests
+﻿using FluentAssertions;
+using TripServiceKata.Entity;
+using Xunit;
+
+namespace TripServiceKata.Tests
 {
     public class TripServiceShould
     {
+        [Fact]
+        public void get_trips_by_user()
+        {
+            var tripService = new TripService();
+            User aGivenUser = new User();
+
+            var trips = tripService.GetTripsByUser(aGivenUser);
+
+            trips.Should().HaveCount(1);
+        }
     }
 }
