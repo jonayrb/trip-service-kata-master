2f7f34b8d3ea7d69cbefed8c0ac6270a9e5571c1
jonayrb <jonayro@hotmail.com>
2021-09-29 08:14:56 +0100

    refactor

--- c#/src/TripServiceKata/Entity/User.cs
+++ c#/src/TripServiceKata/Entity/User.cs
@@ -1,24 +1,19 @@
 ﻿using System.Collections.Generic;
 using TripServiceKata.Exception;
 
-namespace TripServiceKata.Entity
-{
-    public class User : IUser
-    {
-        private List<IUser> friends = new List<IUser>();
+namespace TripServiceKata.Entity {
+    public class User : IUser {
+        private readonly List<IUser> friends = new List<IUser>();
 
-        public List<IUser> GetFriends()
-        {
+        public List<IUser> GetFriends() {
             return friends;
         }
 
-        public void AddFriend(IUser user)
-        {
+        public void AddFriend(IUser user) {
             friends.Add(user);
         }
 
-        public List<Trip> FindTripsByUser()
-        {
+        public List<Trip> FindTripsByUser() {
             throw new DependendClassCallDuringUnitTestException(
                 "TripDAO should not be invoked on an unit test.");
         }
